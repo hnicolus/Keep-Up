edcf2fdc787fb4d7272a71ee8e90a583d3967228
Nicolas <36539498+hnicolus@users.noreply.github.com>
2021-07-07 12:24:47 +0200

    Added Statistics.

---
+++ Frontend/pages/dashboard/Statistics/index.js
@@ -0,0 +1,87 @@
+import React, {useState, useEffect} from "react";
+
+import {Container, Grid} from "@material-ui/core";
+import * as projectService from "../../../services/projectsService";
+import  * as donationService from '../../../services/donationsService';
+import * as contributorService from '../../../services/contributorService';
+import * as eventService from '../../../services/eventService';
+import * as helper from '../../../utils/helpers';
+import MiniCard from "../../../components/dashboard/MiniCard";
+
+function Statistics() {
+
+    const [statsCardsData, setStatsCardsData] = useState([]);
+    const [suburb,setSuburb] = useState('sandown')
+
+    const currentMonth = new Date().getMonth();
+
+    useEffect(()=>{
+        (async ()=>{
+            const data = [...statsCardsData];
+
+            const upComingEvents = await eventService.getUpcoming(suburb);
+            data.push({
+                label:'Up Coming Events',
+                data: upComingEvents.length,
+                icon:'fas fa-calendar-alt'
+            });
+
+            const completedEvents = await eventService.getCompleted(suburb,currentMonth);
+            data.push({
+                label:'Current Month Completed Events',
+                data: completedEvents.length,
+                icon:'far fa-calendar-check'
+            });
+
+            const projects =await projectService.getByMonthAndSuburb(currentMonth,suburb);
+            data.push({
+                label:'Current Month Total Spent',
+                data: `R ${helper.sumOf(projects,'spend')}`,
+                icon:'fas fa-money-bill-wave'
+            });
+
+            const currentMonthCompletedProject = await projectService.getCompleted(suburb,currentMonth);
+            data.push({
+                label:'Current Completed Projects',
+                data: currentMonthCompletedProject.length,
+                icon:'far fa-calendar-check'
+                });
+
+            const upcomingProjects =  await projectService.getUpComing(suburb);
+            data.push({
+                label:'Up Coming Projects',
+                data: upcomingProjects.length,
+                icon:'fas fa-calendar-alt'
+            });
+
+            const donations = await donationService.getByMonthAndSuburb(currentMonth,suburb);
+            data.push({
+                label:'Current Month Total Donation',
+                data: `R ${helper.sumOf(donations,'amount')}`,
+                icon:'fas fa-money-bill-alt'
+            });
+
+            //Todo Contributor should have a contribution Date in order to be filtered
+            const contributors = await contributorService.getBySuburb(suburb);
+            setStatsCardsData(data);
+        })()
+      },[suburb]);
+
+    return (
+        <div>
+            <Container>
+                <Grid container spacing={2}>
+                  {statsCardsData.map((statsData,index) =>(
+                      <Grid key={index} item xs={12} sm={4} md={3}>
+                        <MiniCard  label={statsData.label}
+                                   data={statsData.data}
+                                   icon={statsData.icon} />
+                      </Grid>
+                  ))}
+                </Grid>
+            </Container>
+        </div>
+    );
+}
+
+export default Statistics;
