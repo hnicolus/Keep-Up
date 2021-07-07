65381f334fcda528c0ae463d605b94e5f621b1a3
Nicolas <36539498+hnicolus@users.noreply.github.com>
2021-07-07 22:10:04 +0200

    Added unexpected error logging interceptor.

--- Frontend/services/httpService.js
+++ Frontend/services/httpService.js
@@ -1,9 +1,17 @@
 import axios from "axios";
-const BASE_ENDPOINT = 'https://9pylrbkatk.execute-api.eu-central-1.amazonaws.com/dev';
+const BASE_ENDPOINT = 'https://9pylrbkatk.execute-api.eu-central-1.amazonaws.com/dsev';
 
- // Set config defaults 
+ // Set config defaults
 const http = axios.create({
     baseURL: BASE_ENDPOINT
 });
 
+http.interceptors.response.use(null,error => {
+    const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
+    if (!expectedError)
+    {
+        console.log('Unexpected Error - ',error.message);
+    }
+    return Promise.reject(error);
+})
 export {http } ;
