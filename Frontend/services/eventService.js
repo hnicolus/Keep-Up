640c96727f8d0093044d104eecc0524604f5fe88
Nicolas <36539498+hnicolus@users.noreply.github.com>
2021-07-07 15:27:28 +0200

    added helper filter.

--- Frontend/services/eventService.js
+++ Frontend/services/eventService.js
@@ -1,19 +1,17 @@
 import {http } from './httpService';
 const currentDate = new Date();
 
-export const getByMonthAndSuburb = async (month,year = currentDate.getFullYear(),suburb )=>{
+export const getAllByMonthAndSuburb = async (month, year = currentDate.getFullYear(), suburb )=>{
     try {
-        const results = await getBySuburb(suburb);
-        return results.filter(event => {
-            const itemDate = new Date(event.date);
-            return itemDate.getMonth === month &&  itemDate.getFullYear() === year;
-        })
+        const results = await getAllBySuburb(suburb);
+        return results.filterByMonthAndYear(month,year);
+
     } catch (error) {
         throw new Error(error.message);
     }   
 }
 
-export const getBySuburb = async (suburb)=>{
+export const getAllBySuburb = async (suburb)=>{
     const path = `getEvents?suburb=${suburb}`;
     try {   
             const {data } =await http.get(path);
@@ -25,10 +23,10 @@ export const getBySuburb = async (suburb)=>{
     }
 }
 
-export const getUpcoming =async (suburb)=>{
+export const getAllUpComing =async (suburb)=>{
     try {
         let  result ;
-        result = await getBySuburb(suburb);
+        result = await getAllBySuburb(suburb);
 
         if(result.length >  0 ){
             result = result.filter(event =>new Date(event.date).getTime() > currentDate.getTime());
@@ -44,14 +42,14 @@ export const getCompleted = async(suburb, month = null) =>{
         let result ;
         if(month != null)
         {
-            result = await getByMonthAndSuburb(month,suburb);
+            result = await getAllByMonthAndSuburb(month,suburb);
 
             if(result.length > 0 ){
                 result =  result.filter(event =>new Date(event.date).getDay < currentDate.getDay );
             }
             return  result ;
         }
-        result = await getBySuburb(suburb);
+        result = await getAllBySuburb(suburb);
 
         if(result.length > 0 ){
             return result.filter(event => new Date(event.date).getTime() < currentDate.getTime());
