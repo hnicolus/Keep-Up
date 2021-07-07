57855b1a4e0d0919398e673f07a1502973d5fcc8
Nicolas <36539498+hnicolus@users.noreply.github.com>
2021-07-07 12:22:45 +0200

    Added Statistic Mini Card Component to encapsulate summary Card Data.

---
+++ Frontend/components/dashboard/MiniCard.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { loadCSS } from 'fg-loadcss';
+import {Card, CardContent, Typography,Grid} from "@material-ui/core";
+import Icon from '@material-ui/core/Icon';
+
+const MiniCard =({label,data,icon})=>{
+
+    React.useEffect(() => {
+        const node = loadCSS(
+            'https://use.fontawesome.com/releases/v5.12.0/css/all.css',
+            document.querySelector('#font-awesome-css'),
+        );
+        return () => {
+            node.parentNode.removeChild(node);
+        };
+    }, []);
+    return(
+        <Card>
+            <CardContent>
+                <Grid container spacing={2} alignItems='center' justify='center'>
+                    <Grid item sm={3}>
+                        <Typography align='center'>
+                            <Icon fontSize='large' className={icon} color="primary"/>
+                        </Typography>
+                    </Grid>
+                    <Grid item sm={9}>
+                        <Typography align='center' style={{fontWeight:'bold'}}>{label}</Typography>
+                        <Typography align='center'>{data}</Typography>
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    )
+}
+export  default  MiniCard;
