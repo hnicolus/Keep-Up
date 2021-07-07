58157a3059210286de44c3cad7d99e45eb6f965f
Nicolas <36539498+hnicolus@users.noreply.github.com>
2021-07-07 12:21:09 +0200

    Added  Projects Service to communicate with project backend API.

---
+++ Frontend/services/projectsService.js
@@ -0,0 +1,65 @@
+import {http } from './httpService';
+
+
+const currentDate = new Date();
+
+export const getByMonthAndSuburb=async (month,suburb)=>{
+
+    try {
+        const results =await  getBySuburb(suburb);
+
+        return results.filter(project =>new Date(project.date).getMonth() === month);
+
+    } catch (error) {
+        throw new Error(error.message)
+    }
+}
+
+export const getBySuburb = async(suburb)=>{
+
+    const projects_url = `/getProjects?suburb=${suburb}`;
+    try {
+        const result =await http.get(projects_url);
+        return result.data;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+export const getUpComing = async(suburb)=>{
+    
+    try {
+        const projects =await getBySuburb(suburb);
+        return projects.filter(project => new Date(project.transactionDate).getTime() > currentDate.getTime());
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+export const  getCompleted = async(suburb, month = null)=>{
+    
+    try {
+        let results ;
+
+        if(month != null){
+            results = await getByMonthAndSuburb(suburb,month) ;
+
+            if( results.length > 0 ){
+                results = results.filter(event => new Date(event.Date).getMonth() === month);
+            }
+
+        }else{
+            results =await getBySuburb(suburb);
+        }
+        if( results.length > 0 ){
+            return projects.filter(project =>{
+            const projectDate = new Date(project.transactionDate);
+            return projectDate.getTime() < currentDate.getTime() && projectDate.getFullYear() === currentDate.getFullYear(); 
+        });
+    }
+
+        return results;
+    } catch (error) {
+        throw new Error(error.message);
+    }   
+}
