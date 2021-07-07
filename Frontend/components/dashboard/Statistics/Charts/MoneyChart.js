7eb782b8af8ca3881b01edf51603c7c6d7ab7027
Nicolas <36539498+hnicolus@users.noreply.github.com>
2021-07-08 01:02:40 +0200

    Added Skeleton.

--- Frontend/components/dashboard/Statistics/Charts/MoneyChart.js
+++ Frontend/components/dashboard/Statistics/Charts/MoneyChart.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Bar } from 'react-chartjs-2';
-import {Card, CardContent, Typography} from "@material-ui/core";
+import {Bar} from 'react-chartjs-2';
+import {Card, CardContent, Grid, Typography} from "@material-ui/core";
+import {Skeleton} from "@material-ui/lab";
 
 const options = {
     scales: {
@@ -13,29 +14,50 @@ const options = {
         ],
     },
 };
-const MoneyChart = ({months, moneyReceived,moneySpent, moneySpentColor,moneyReceivedColor}) =>{
+
+const displaySkeleton = () => {
+    return (
+        <Card>
+            <CardContent>
+                <div className='header'>
+                    <Typography variant='h6'><Skeleton variant='text' /></Typography>
+                </div>
+                <Grid container justify='center' >
+                    <Grid item sm={12} xs={12} md={12} style={{paddingTop:'10px',paddingBottom:'10px'}} >
+                            <Skeleton height={270} width='100%' variant='rect'/>
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    )
+}
+
+const MoneyChart = ({months, moneyReceived, moneySpent, moneySpentColor, moneyReceivedColor,loading}) => {
     const data = {
         labels: months,
         datasets: [
             {
                 label: '# Money (R) Spent',
-                data:  [...moneySpent],
+                data: [...moneySpent],
                 backgroundColor: moneySpentColor,
             },
             {
                 label: '# Money (R) Received',
-                data:[...moneyReceived],
+                data: [...moneyReceived],
                 backgroundColor: moneyReceivedColor,
             },
         ],
     };
+
+    if(loading) return displaySkeleton();
+
     return (
         <Card>
             <CardContent>
                 <div className='header'>
                     <Typography variant='h6'>Money received and Money spent</Typography>
                 </div>
-                <Bar  data={data} options={options} />
+                <Bar data={data} options={options}/>
             </CardContent>
         </Card>
     );
